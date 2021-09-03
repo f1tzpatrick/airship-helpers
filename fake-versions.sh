b75be963f7b81909a6b993a332fabe3cf7d03797
Steven Fitzpatrick <steven@fitz.gg>
2021-09-03 20:33:02 +0000

    Add repository versions to fake-versions.sh

--- fake-versions.sh
+++ fake-versions.sh
@@ -5,3 +5,4 @@
 sed -ri "s/(image:\s)(.*)/\1IMAGE/g" $HOME/treasuremap/manifests/function/treasuremap-base-catalogues/versions-treasuremap.yaml
 sed -ri "s/(tag:\s)(.*)/\1TAG/g" $HOME/treasuremap/manifests/function/treasuremap-base-catalogues/versions-treasuremap.yaml
 sed -ri "s/(version:\s)(.*)/\1VERSION/g" $HOME/treasuremap/manifests/function/treasuremap-base-catalogues/versions-treasuremap.yaml
+sed -ri "s/(repository:\s)(.*)/\1REPOSITORY/g" $HOME/treasuremap/manifests/function/treasuremap-base-catalogues/versions-treasuremap.yaml
